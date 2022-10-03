b66da8e75f87b660fbb3ec2ca840c744efec8f15
HoatFPT-APTECH <hoatmhth2106010@fpt.edu.vn>
2022-10-03 16:50:33 +0700

    up date
    hoat , bai truoc bi loi thay ạ

--- LapTrinh/Bai_3.cpp
+++ LapTrinh/Bai_3.cpp
@@ -1,11 +1,46 @@
 #include<stdio.h>
-bool input(long *arr[][]){
-   int n,k;
-   scanf("%d%d",&n,&k);
-   if(!(n>0&&n<800&&k<=40))return false;
-   for(int i=0;i<n;i++){
-   	for(int j=0;j<n;j++){
+bool input(int *arr[][100],int m , int k){
+  
+   if(!(m>0&&m<800&&k<=40))return false;
+   for(int i=0;i<m;i++){
+   	for(int j=0;j<m;j++){
    		scanf("%ld",arr[i][j]);
 	   }
    }
+} 
+bool check(int x , int y, int arr[100][100],int k){// x la vi trih hang bat dau duyet
+// y la vi tri bat dau duyet theo cot.
+// arr la mang tham chieu
+// k la do dai mang con can duyet
+	for(int m=x;m<=x+k-1;m++){
+		for(int n=y;n<=y+k-1;n++){
+			if(arr[m][n]==0) return false;
+		}
+	}
+	return true;
 }
+int main(){
+	int m,k,arr[100][100];
+	scanf("%d%d",&m,&k);
+
+// if(!(m>0&&m<800&&k<=40)) {
+// 	printf("-1");
+// 	return 0;
+// };
+   for(int i=0;i<m;i++){
+   	for(int j=0;j<m;j++){
+   		scanf("%d",&arr[i][j]);
+	   }
+   }
+   
+    int count=0;
+    for(int i=0;i<m-k+1;i++){
+    	for(int j=0;j<m-k+1;j++){
+    		if(check(j,i,arr,k)==true)count++;
+		}
+	}
+	
+	printf("%d",count);
+    
+}
+
